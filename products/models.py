744cc2414244caff8e7f522305bc83f85cbfdd34
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2023-01-04 11:55:18 -0300

    Fix - models

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from categories.models import Category
+from accounts.models import Account
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -9,3 +10,4 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     amount = models.IntegerField(null=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
