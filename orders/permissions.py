5ff81bed8cad74588144dc4626400fb17da6bc01
Enrique <enrique.barbosasilva@gmail.com>
2023-01-06 14:58:20 -0300

    feat: orders-permissions

---
+++ orders/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+from rest_framework.views import View, Request
+from .models import Order
+
+
+class IsManagerOrOrderOwner(permissions.BasePermission):
+    def has_object_permission(self, request:Request, view:View, obj:Order):
+        return obj.account == request.user or request.user.is_superuser
