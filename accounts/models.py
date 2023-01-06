f6e20334a131b1035cf07ccfc25ab51868b17512
Yuri Bentes <eypbentes@gmail.com>
2023-01-06 11:17:08 -0300

    feat:accounts

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Account(AbstractUser):
-    ...
+    name: models.CharField(max_length=100, unique=True)
+    cpf: models.CharField(max_length=11, unique=True)
+    email: models.CharField(max_length=100, null=False)
+    telephone: models.CharField(max_length=12)
+    password: models.CharField(max_length=100, null=False)
+    is_superuser: models.BooleanField(default=False)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from .models import Account
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import AccountSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class AccountView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+
+class AccountDetailedView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]  
+
+    serializer_class = AccountSerializer
+    queryset = Account.objects.all()
+
+    lookup_url_kwarg = "pk"
+
+
+
+
+
+    
+# class AccountView(ListCreateAPIView):
+#     authentication_classes = [JWTAuthentication]
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = AccountSerializer
+
+#     def get_queryset(self):
+#         return Account.objects.filter(id=self.kwargs["pk"])
+        
+
+#     def perform_create(self, serializer):
+
+#         account = get_object_or_404(Account, id=self.kwargs["pk"])
+#         serializer.save(account)
+    
